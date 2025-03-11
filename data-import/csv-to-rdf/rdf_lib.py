e23a1bf94fc2c6a2cf8799a9475afef31e669c46
rderbier <rderbier@gmail.com>
2025-03-11 20:19:40 +0100

    Update rdf_lib.py
    chnage function handling

--- data-import/csv-to-rdf/rdf_lib.py
+++ data-import/csv-to-rdf/rdf_lib.py
@@ -1,6 +1,7 @@
 import random
 import re
 import sys
+import json
 from datetime import datetime, timedelta
 
 sliceSize = 5000  # mutate every sliceSize RDF lines
@@ -121,24 +122,41 @@ def substituteFunctions(match_obj, row):
     if match_obj.group() is not None:
         func = match_obj.group(2)
         if func == "geoloc":
-            lat = float(match_obj.group(3))
-            lng = float(match_obj.group(4))
+            params = match_obj.group(3).split(",")
+            lat = float(params[0])
+            lng = float(params[1])
             # fmt.Sprintf("\"{\\type\\\":\\\"Point\\\",\\\"coordinates\\\":[%s,%s]}\"^^<geo:geojson>", opMatch[2], opMatch[3]
-            return match_obj.group(1)+f"\"{{'type':'Point','coordinates':[{lng:.8f},{lat:.8f}]}}\"^^<geo:geojson>"
+            return match_obj.group(1)+f"\"{{'type':'Point','coordinates':[{lng:.8f},{lat:.8f}]}}\"^^<geo:geojson>"++match_obj.group(4)
         elif func == "datetime":
-            date_string = match_obj.group(3)
-            format = match_obj.group(4)
+            params = match_obj.group(3).split(",")
+            date_string = params[0]
+            format = params[1]
             date = datetime.strptime(date_string, format)
-            return match_obj.group(1)+'"'+date.strftime("%Y-%m-%dT%H:%M:%S")+'"'
+            return match_obj.group(1)+'"'+date.strftime("%Y-%m-%dT%H:%M:%S")+'"'+match_obj.group(4)
         elif func == "randomDate":
-            start = datetime.strptime(match_obj.group(3), "%Y-%m-%d")
-            end = datetime.strptime(match_obj.group(4), "%Y-%m-%d")
+            params = match_obj.group(3).split(",")
+            date_string = params[0]
+            format = params[1]
+            start = datetime.strptime(params[0], "%Y-%m-%d")
+            end = datetime.strptime(params[1], "%Y-%m-%d")
             # Generate a random number of days between start and end
             random_days = random.randint(0, (end - start).days)
             # Add the random number of days to the start date
             random_date = start + timedelta(days=random_days)
             # Return the date in the desired format
-            return match_obj.group(1)+'"'+random_date.strftime("%Y-%m-%d")+'"'
+            return match_obj.group(1)+'"'+random_date.strftime("%Y-%m-%d")+'"'+match_obj.group(4)
+        elif func == "split":
+            params = match_obj.group(3).strip()
+            r = ""
+            if (params.startswith('[') & params.endswith(']')):
+                params = params.replace("'",'"')
+                values = json.loads(params)
+                for v in values:
+                    r += (match_obj.group(1)+'"'+v.strip()+'"'+match_obj.group(4))+"\n"
+            else:
+                r = match_obj.group(1)+'"'+params+'"'+match_obj.group(4)+"\n"
+            return r
+
         else:
             raise ValueError("unsupported function " + func)
 
@@ -147,11 +165,10 @@ re_column = re.compile(r"(\[[\w .,|]+\])")
 # re_uid = re.compile(r"<[^[]*(\[[\w .,|]+\])[^>]*>")
 re_uid = re.compile(r"<_:[^>]*?(\[[^\]]+\])(?:[^>]*?(\[[^\]]+\]))?[^>]*?>")
 # =func(param1,param2) not in a string
-re_functions = re.compile(r'(^[^"]*)=(\w+)\(([^,)]+),?([^,)]+)?\)')
+re_functions = re.compile(r'(^[^"]*)=(\w+)\(([^)]+)?\)(.*)$')
 
 
 def substituteInTemplate(template, row):
-    
     fields = re_column.findall(template)
     for field in fields:
         column = field[1:-1].split(",")[0]
@@ -182,7 +199,8 @@ def transformDataFrame(df, template):
         for rdftemplate in valid_templates:
             rdf = substituteInTemplate(rdftemplate, row)
             if rdf is not None:
-                addRdfToMap(rdf_map, rdf)
+                for r in rdf.split("\n"):
+                    addRdfToMap(rdf_map, r)
 
     # Apply processing row-wise
     df.apply(process_row, axis=1)
