d466de0917c51254d103c17ac58d4f81a5aa53c2
rderbier <rderbier@gmail.com>
2025-03-17 23:40:01 +0100

    Update test.py
    example using serverless dgraph

--- knowledge-graph/samples/unstructured/test.py
+++ knowledge-graph/samples/unstructured/test.py
@@ -7,8 +7,12 @@ from KGkit import KG
 dotenv.load_dotenv()
 # Add a .env file to the root of the project with the following content:
 # MISTRAL_API_KEY=YOUR_API_KEY
+# DGRAPH_GRPC=YOUR_DGRAPH_GRPC_ENDPOINT
+# DGRAPH_TOKEN=YOUR_DGRAPH_TOKEN
 
-kg = KG().with_mistral("mistral-large-latest",Mistral(api_key=os.getenv("MISTRAL_API_KEY")))
+# kg=KG() for localhost:9080 dgraph instance
+kg = KG(grpc_target=os.getenv("DGRAPH_GRPC"), token=os.getenv("DGRAPH_TOKEN"))\
+.with_mistral("mistral-large-latest",Mistral(api_key=os.getenv("MISTRAL_API_KEY")))
 
 # Declare a GraphQL Data model for the knowledge graph
 # The schema is written in the Dgraph GraphQL schema syntax
@@ -21,13 +25,15 @@ kg.with_kg_schema('''
                     name: String! @id
                     """ description/benefits of the food product."""
                     description: String
+                    """ List of businesses that provide this food product."""
+                    providers: [LocalBusiness] @hasInverse(field: products)
                   }
                   """A particular physical business or branch of an organization"""
                   type LocalBusiness {
                     """ Short name uniquely identifying the organization."""
                     name: String! @id
                     """ List of food products provided by this business."""
-                    products: [FoodProduct]
+                    products: [FoodProduct] @hasInverse(field: providers)
                     """ contact email of the organization."""
                     contact_email: String
                     """ contact phone number of the organization."""
